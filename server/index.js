de42e2dffa16e4d91317777242f2b126c609e72e
Muhammed-Hagras <40708938+Muhammed-Hagras@users.noreply.github.com>
2023-05-09 09:25:08 +0200

    update scripts

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const products = require('./products');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const products = require("./products");
 const register = require("./routes/register");
 const login = require("./routes/login");
 const stripe = require("./routes/stripe");
@@ -16,33 +16,33 @@ require("dotenv").config();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/register", register)
+app.use("/api/register", register);
 app.use("/api/login", login);
-app.use("/api/stripe", stripe);  
+app.use("/api/stripe", stripe);
 app.use("/api/products", productRouter);
 app.use("/api/users", users);
 app.use("/api/orders", orders);
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.get('/products', (req, res) => {
-    res.send(products);
+app.get("/products", (req, res) => {
+  res.send(products);
 });
 
-const DB = process.env.MONGO_URL
+const DB = process.env.MONGO_URL;
 const port = process.env.port || 8000;
 
 mongoose
-.connect(DB)
-.then(()=>{console.log("DB Connected succefully")})
-.catch((error) =>{ console.log("MongoDB Connection failed...."+error);});
+  .connect(DB)
+  .then(() => {
+    console.log("DB Connected succefully");
+  })
+  .catch((error) => {
+    console.log("MongoDB Connection failed...." + error);
+  });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}...`);
+  console.log(`Server running on port ${port}...`);
 });
-
-
-
